fix minor bug when loading raft, apparrently the model is on gpu, put on cpu explicitly. No memory leakage now

src/models/raft.py
@@ -165,7 +165,7 @@ def raft_pretrained():
     # pathmgr = PathManager()
     # pathmgr.register_handler(ManifoldPathHandler(), allow_override=True)
     # model.load_state_dict(torch.load(pathmgr.get_local_path(RAFT_PRETRAINED)))
-    model.load_state_dict(torch.load(RAFT_PRETRAINED))
+    model.load_state_dict(torch.load(RAFT_PRETRAINED, map_location="cpu"))
     model = model.module
     model.eval()
     return model