c5c70ef03ad8e7428c0d8b236f84986963903e91
AndreiDavydov <andrey0davydov@gmail.com>
2023-02-28 15:09:46 +0100

    up optical flow 2d loss with thresholding and normalization

--- src/losses/optical_flow.py
+++ src/losses/optical_flow.py
@@ -2,8 +2,32 @@ import torch
 
 
 class Flow2DLoss(torch.nn.Module):
-    def __init__(self):
+    """
+    Two important parts: thresholding and normalization.
+
+    Thresholding:
+    Some of the pairs of frames are not valid 
+    (due to severe occlusions or wrong frame cuts).
+    Such samples can be discarded via thresholding of OF norm. 
+    For every sample (pair of frames) in the batch one can compute OF norms. 
+    The maximum value across all vertices seems to be a good thresholding.
+    threshold by max: 50
+    threshold by mean: 30 (tested on 3DPW-test samples)
+
+    Normalization:
+    Different pairs of frames from one or from different datasets might have
+    very different motions (both body flow and OF). 
+    Hence, it is important to equalize them for stable training.
+    We propose to normalize by OF norm means (across all visible vertices).
+
+    """
+    def __init__(self, normalize_OF=False, do_thresholding=True, maxOF_threshold=50, reduction="mean"):
         super(Flow2DLoss, self).__init__()
+        self.normalize_OF = normalize_OF
+        self.do_thresholding = do_thresholding
+        self.maxOF_threshold = maxOF_threshold
+        self.reduction = reduction
+
 
     def forward(self, verts2d_flow_pred, optical_flow_2d_unproj, vis_mask):
         """
@@ -16,10 +40,40 @@ class Flow2DLoss(torch.nn.Module):
             print("vis_mask is zero!")
             return verts2d_flow_pred[0,0,0] * 0.
 
-        err = (verts2d_flow_pred - optical_flow_2d_unproj) ** 2
+        of_norm = optical_flow_2d_unproj.norm(dim=-1)  # B x N
+        of_norm = of_norm * vis_mask  # B x N
+
+        ### THRESHOLDING: mask bad OF values (might be due to wrong pairs of frames in a dataset)
+        if self.do_thresholding:
+            of_norm_max = of_norm.max(dim=-1).values  # B
+            of_mask = torch.arange(of_norm_max.size(0)).to(of_norm_max.device)
+            of_mask = of_mask[of_norm_max < self.maxOF_threshold]
+
+            of_norm = of_norm[of_mask]
+            verts2d_flow_pred = verts2d_flow_pred[of_mask]
+            optical_flow_2d_unproj = optical_flow_2d_unproj[of_mask]
+            vis_mask = vis_mask[of_mask]
+        
+        err = (verts2d_flow_pred - optical_flow_2d_unproj) ** 2  # B x N
         err = err.sum(dim=-1)  # B x N
-        err = err * vis_mask
-        return err.sum() / vis_mask.sum()
+        err = err * vis_mask  # B x N
+        err = err.sum(dim=-1) / vis_mask.sum(dim=-1) # B
+        
+        ### NORMALIZATION: compute mean across all visible vertices per every sample
+        if self.normalize_OF:
+            of_norm_mean = of_norm.sum(dim=-1) / vis_mask.sum(dim=-1) # B
+            s = of_norm_mean ** 2 # as error is ^2
+            err = err / s.detach()  # B
+        
+        if sum(torch.isnan(err)) > 0:
+            return verts2d_flow_pred[0,0,0] * 0.
+
+        if self.reduction is None:
+            return err
+        elif self.reduction == "mean":
+            return err.mean()
+
+
 
 
 # class Flow3DLoss(torch.nn.Module):
